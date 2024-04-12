c1e723e64485557b22e8d9ed53ccdc2256dc42cb
10kdream <97788884+10kdream@users.noreply.github.com>
2024-04-11 17:41:19 -0700

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -1593,15 +1593,16 @@ new BlazeSlider(el, {
               </span>
               <span className="home-text529">Rob Kinelski</span>
               <span className="home-text530">
-                , Smith is a rising talent set apart by her perspective having
-                being immersed in music scenes across the globe. From Brisbane,
-                to London, to Berlin, to Los Angeles.
+                , Smith is a rising talent set apart by her perspective being
+                immersed in music scenes across the globe. From Brisbane, to
+                London, to Berlin, to Los Angeles.
               </span>
               <br className="home-text531"></br>
               <br className="home-text532"></br>
               <span className="home-text533">
                 Growing up in Australia, Smith cured small-town-boredom getting
-                her first DAW at age 12, setting in motion a lifelong obsession.
+                her first DAW at age 12, setting in motion a lifelong obsession
+                with sound.
                 <span
                   dangerouslySetInnerHTML={{
                     __html: ' ',
@@ -2123,18 +2124,12 @@ new BlazeSlider(el, {
           />
           <div className="home-container143">
             <span className="home-text733">
-              <span className="home-text734">  RUBY SMITH</span>
-              <br></br>
-              <span>AUDIO ENGINEER</span>
-              <br></br>
-            </span>
-            <span className="home-text738">
               <br></br>
               <span>Stay in touch</span>
               <br></br>
             </span>
-            <span className="home-text742">
-              <span className="home-text743">For all enquiries:</span>
+            <span className="home-text737">
+              <span className="home-text738">For all enquiries:</span>
               <span>
                 <span
                   dangerouslySetInnerHTML={{
@@ -2171,6 +2166,12 @@ new BlazeSlider(el, {
                 <path d="M135.429 808h132v-396.571h-132v396.571zM276 289.143c-0.571-38.857-28.571-68.571-73.714-68.571s-74.857 29.714-74.857 68.571c0 37.714 28.571 68.571 73.143 68.571h0.571c46.286 0 74.857-30.857 74.857-68.571zM610.286 808h132v-227.429c0-121.714-65.143-178.286-152-178.286-70.857 0-102.286 39.429-119.429 66.857h1.143v-57.714h-132s1.714 37.143 0 396.571v0h132v-221.714c0-11.429 0.571-23.429 4-32 9.714-23.429 31.429-48 68-48 47.429 0 66.286 36 66.286 89.714v212zM877.714 237.714v548.571c0 90.857-73.714 164.571-164.571 164.571h-548.571c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571c90.857 0 164.571 73.714 164.571 164.571z"></path>
               </svg>
             </a>
+            <span className="home-text742">
+              <span className="home-text743">  RUBY SMITH</span>
+              <br></br>
+              <span>AUDIO ENGINEER</span>
+              <br></br>
+            </span>
           </div>
         </div>
       </section>
